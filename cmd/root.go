88bf4f96ee475b96b392a427b9f5e8fe7e9a9ded
Curtis Jewell <golang@curtisjewell.name>
2023-12-29 21:56:53 -0700

    feat: Implementing more functionality

--- cmd/root.go
+++ cmd/root.go
@@ -31,6 +31,7 @@ import (
 	"path"
 	"slices"
 	"sort"
+	"strings"
 
 	git "github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
@@ -47,7 +48,8 @@ import (
 type VersionSegment int
 
 const (
-	versionMajor VersionSegment = iota
+	versionNone VersionSegment = iota
+	versionMajor
 	versionMinor
 	versionPatch
 	versionAlpha
@@ -57,7 +59,7 @@ const (
 	versionPre
 )
 
-var vsName = []string{"major", "minor", "patch", "alpha", "beta", "gamma", "rc", "pre"}
+var vsName = []string{"", "major", "minor", "patch", "alpha", "beta", "gamma", "rc", "pre"}
 
 func (vs VersionSegment) String() string {
 	return vsName[vs]
@@ -71,12 +73,14 @@ var (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:     "git next-tag",
-	Version: FullVersion(),
-	Short:   "Commit the next tag.",
-	Long:    `...`,
-	PreRunE: func(cmd *cobra.Command, args []string) error { return initConfig() },
-	RunE:    func(cmd *cobra.Command, args []string) error { return nextTag(cmd, args) },
+	Use:                        "git next-tag",
+	Version:                    FullVersion(),
+	Short:                      "Commit the next tag.",
+	Long:                       `...`,
+	SilenceUsage:               true,
+	PreRunE:                    func(cmd *cobra.Command, args []string) error { return initConfig() },
+	RunE:                       func(cmd *cobra.Command, args []string) error { return nextTag(cmd, args) },
+	SuggestionsMinimumDistance: 5,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -128,7 +132,7 @@ func initConfig() error {
 		// Initialize the file.
 		// TODO: Make this a 'question and answer' bit
 		viper.Set("initial_v", true)
-		viper.Set("tag_annotated", true)
+		viper.Set("tag_annotated", false)
 		viper.Set("version_files", []string{})
 		viper.Set("always_leave_version_pre", true)
 		err = viper.WriteConfig()
@@ -156,40 +160,22 @@ func nextTag(_ *cobra.Command, _ []string) error {
 		}
 	*/
 
-	//revive:disable:add-constant 100 is an only-used-once size
-	tags := make(map[string]*object.Tag)
-	tagNames := make([]string, 0, 100)
-	//revive:enable:add-constant
-
-	// Start by checking if there are any tags
-	iter, err := repo.Tags()
+	tags, err := retrieveTags()
 	if err != nil {
 		return err
 	}
 
-	if err := iter.ForEach(func(ref *plumbing.Reference) error {
-		obj, err := repo.TagObject(ref.Hash())
-		switch err {
-		case nil:
-			tags[obj.Name] = obj
-			tagNames = append(tagNames, obj.Name)
-		case plumbing.ErrObjectNotFound:
-			// Not a tag object
-		default:
-			// Some other error
-			return err
-		}
-		return nil
-	}); err != nil {
-		return err
-	}
-
-	if len(tagNames) == 0 {
+	if len(tags) == 0 {
 		initialTag, err := askInitialTagging()
 		if err != nil {
 			return err
 		}
 		return doTagging(initialTag)
+	}
+
+	tagNames := make([]string, 0, len(tags))
+	for k := range tags {
+		tagNames = append(tagNames, k)
 	}
 
 	tagVersions := make(map[string]*ParsedVersion)
@@ -198,6 +184,140 @@ func nextTag(_ *cobra.Command, _ []string) error {
 		tagVersions[s] = pv
 	}
 
+	tagNames = sortTags(tagNames, tagVersions)
+
+	currentVersion := tagNames[0]
+	slog.Debug("Current version: " + currentVersion)
+
+	currentParsedVersion := tagVersions[currentVersion]
+
+	nextVersion, err := getSpecifiedVersion(currentParsedVersion)
+	if err != nil {
+		return err
+	}
+
+	//revive:disable:unhandled-error
+	fmt.Println(nextVersion)
+	//revive:enable:unhandled-error
+
+	// getRecommendedVersion(tags[currentVersion])
+
+	/*
+
+	   VERSION=""
+
+	   if [[ $VERSION == 'major' ]]
+	   then
+	     VNUM1=$((VNUM1+1))
+	     VNUM2=0
+	     VNUM3=0
+	   elif [[ $VERSION == 'minor' ]]
+	   then
+	     VNUM2=$((VNUM2+1))
+	     VNUM3=0
+	   elif [[ $VERSION == 'patch' ]]
+	   then
+	     VNUM3=$((VNUM3+1))
+	   else
+	     echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+	     exit 1
+	   fi
+
+	   #create new tag, putting back the v.
+	   NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
+	   echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
+
+	   #get current hash and see if it already has a tag
+	   GIT_COMMIT=`git rev-parse HEAD`
+	   NEEDS_TAG=`git describe --tags --contains $GIT_COMMIT 2>/dev/null`
+
+	   #only tag if no tag already
+	   if [ -z "$NEEDS_TAG" ]; then
+	     git stash
+	     git tag $NEW_TAG
+	     echo "Tagged with $NEW_TAG"
+	     git push
+	     git push --tags
+	   else
+	     echo "Already a tag on this commit"
+	   fi
+
+	   exit 0
+
+
+	*/
+
+	return nil
+}
+
+func retrieveTags() (map[string]*object.Tag, error) {
+	tags := make(map[string]*object.Tag)
+
+	// Start by checking if there are any tags
+	iter, err := repo.Tags()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := iter.ForEach(func(ref *plumbing.Reference) error {
+		obj, err := repo.TagObject(ref.Hash())
+		switch err {
+		case nil:
+			tags[obj.Name] = obj
+		case plumbing.ErrObjectNotFound:
+			// Not a tag object
+		default:
+			// Some other error
+			return err
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+func getSpecifiedVersion(currentPV *ParsedVersion) (*ParsedVersion, error) {
+	var pv ParsedVersion
+	if viper.GetBool("major") {
+		pv = ParsedVersion{
+			major:         currentPV.major + 1,
+			minor:         0,
+			patch:         0,
+			lower:         0,
+			lowerCategory: versionNone,
+			isPre:         false,
+		}
+		return &pv, nil
+	}
+	if viper.GetBool("minor") {
+		pv = ParsedVersion{
+			major:         currentPV.major,
+			minor:         currentPV.minor + 1,
+			patch:         0,
+			lower:         0,
+			lowerCategory: versionNone,
+			isPre:         false,
+		}
+		return &pv, nil
+	}
+	if viper.GetBool("patch") {
+		pv = ParsedVersion{
+			major:         currentPV.major,
+			minor:         currentPV.minor,
+			patch:         currentPV.patch + 1,
+			lower:         0,
+			lowerCategory: versionNone,
+			isPre:         false,
+		}
+		return &pv, nil
+	}
+
+	return nil, fmt.Errorf("Unfinished")
+}
+
+func sortTags(tagNames []string, tagVersions map[string]*ParsedVersion) []string {
 	sort.Slice(tagNames, func(i, j int) bool {
 		tvj := tagVersions[tagNames[j]]
 		if tvj == nil {
@@ -252,56 +372,7 @@ func nextTag(_ *cobra.Command, _ []string) error {
 
 	// To turn the slice around so that the greatest versions are first
 	slices.Reverse(tagNames)
-
-	currentVersion := tagNames[0]
-	slog.Info("Current version: " + currentVersion)
-
-	/*
-
-	   VERSION=""
-
-	   if [[ $VERSION == 'major' ]]
-	   then
-	     VNUM1=$((VNUM1+1))
-	     VNUM2=0
-	     VNUM3=0
-	   elif [[ $VERSION == 'minor' ]]
-	   then
-	     VNUM2=$((VNUM2+1))
-	     VNUM3=0
-	   elif [[ $VERSION == 'patch' ]]
-	   then
-	     VNUM3=$((VNUM3+1))
-	   else
-	     echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
-	     exit 1
-	   fi
-
-	   #create new tag, putting back the v.
-	   NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
-	   echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
-
-	   #get current hash and see if it already has a tag
-	   GIT_COMMIT=`git rev-parse HEAD`
-	   NEEDS_TAG=`git describe --tags --contains $GIT_COMMIT 2>/dev/null`
-
-	   #only tag if no tag already
-	   if [ -z "$NEEDS_TAG" ]; then
-	     git stash
-	     git tag $NEW_TAG
-	     echo "Tagged with $NEW_TAG"
-	     git push
-	     git push --tags
-	   else
-	     echo "Already a tag on this commit"
-	   fi
-
-	   exit 0
-
-
-	*/
-
-	return nil
+	return tagNames
 }
 
 func askInitialTagging() (string, error) {
@@ -330,6 +401,60 @@ func askInitialTagging() (string, error) {
 	return versionInitial, nil
 }
 
+func replaceInFile(fileName, newVersion string) error {
+	input, err := os.ReadFile(fileName)
+	if err != nil {
+		return fmt.Errorf("Could not read file %s: %w", fileName, err)
+	}
+
+	fi, err := os.Stat(fileName)
+	if err != nil {
+		return fmt.Errorf("Could not get information about file %s: %w", fileName, err)
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		finds := rxVersion.FindAllString(line, -1)
+		finds = slices.Compact(finds)
+		if len(finds) != 0 {
+			for _, find := range finds {
+				if find != newVersion {
+					line = strings.Replace(line, find, newVersion, -1)
+				}
+			}
+
+			lines[i] = line
+		}
+	}
+
+	output := strings.Join(lines, "\n")
+	err = os.WriteFile(fileName, []byte(output), fi.Mode().Perm())
+	if err != nil {
+		return fmt.Errorf("Could not write to file %s: %w", fileName, err)
+	}
+
+	return nil
+}
+
+func createVersionDotGoFile(pkg, fileName string) error {
+	versionFile := `
+package ` + pkg + `
+
+// Version is the current version of the library or command
+var Version = func() string { return "v0.1.0" }()
+`
+
+	//revive:disable:add-constant
+	err := os.WriteFile(fileName, []byte(versionFile), 0644)
+	//revive:enable:add-constant
+	if err != nil {
+		return fmt.Errorf("Could not write to file %s: %w", fileName, err)
+	}
+
+	return nil
+}
+
 // This is my 'TODO' area of functions I think I'll need soon.
 //
 //revive:disable:unused-parameter
@@ -341,10 +466,10 @@ func doTagging(tag string) error {
 	return errors.New("Tagging not implemented yet")
 }
 
-func replaceInFile(fileName, newVersion string) error {
-	return errors.New("replaceInFile not implemented yet")
-}
+func getRecommendedVersion(tag *object.Tag) error {
+	// headRef, err := repo.Reference(plumbing.HEAD, true)
 
-func createFile(fileName string) error {
-	return errors.New("createFile not implemented yet")
+	// initialCommit, err := tag.Commit()
+
+	return fmt.Errorf("getRecommendedVersion not implemented yet")
 }
